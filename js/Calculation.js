ad2996e74adc3551e54d66977d458665ec396f73
Christoph Zirkelbach <christoph.zirkelbach@htw-dresden.de>
2022-02-01 21:24:10 +0100

    Initial import

---
+++ js/Calculation.js
@@ -0,0 +1,80 @@
+class Calculation {
+
+    constructor() {
+        this.gradeGutachten1 = new Grade();
+        this.gradeGutachten2 = new Grade();
+        this.gradeVortrag = new Grade();
+        this.gradeDiskussion = new Grade();
+        this.variant = 1;
+
+        this.gradeSchriftlicheArbeit = new Grade();
+        this.gradeVerteidigung = new Grade();
+        this.gradeAbschlussarbeit = new Grade();
+        this.gradeAbschlussarbeitAsText = '';
+    }
+
+    setInputGrades(grade1, grade2, grade3, grade4, variant = 1) {
+        this.gradeGutachten1.value = Grade.floatToInt(parseFloat(grade1));
+        this.gradeGutachten2.value = Grade.floatToInt(parseFloat(grade2));
+        this.gradeVortrag.value = Grade.floatToInt(parseFloat(grade3));
+        this.gradeDiskussion.value = Grade.floatToInt(parseFloat(grade4));
+        this.variant = variant;
+
+        this.calc();
+    }
+
+    calc() {
+        this.calcGradeSchriftlicheArbeit();
+        this.calcGradeVerteidigung();
+        this.calcGradeAbschlussarbeit();
+    }
+
+    calcGradeSchriftlicheArbeit() {
+        if (this.gradeGutachten1.value == 0 || this.gradeGutachten2.value == 0) {
+            this.gradeSchriftlicheArbeit.value = 0;
+        } else {
+            if (this.gradeGutachten1.value > (4 * Grade.precisionFactor) || this.gradeGutachten2.value > (4 * Grade.precisionFactor)) {
+                this.gradeSchriftlicheArbeit.value = (5 * Grade.precisionFactor);
+            } else {
+                const result = (this.gradeGutachten1.value + this.gradeGutachten2.value) / 2;
+                this.gradeSchriftlicheArbeit.value = parseInt(result);
+            }
+        }
+    }
+
+    calcGradeVerteidigung() {
+        if (this.gradeVortrag.value == 0 || this.gradeDiskussion.value == 0) {
+            this.gradeVerteidigung.value = 0;
+        } else {
+            const result = (this.gradeVortrag.value + this.gradeDiskussion.value) / 2;
+            this.gradeVerteidigung.value = (result > (4 * Grade.precisionFactor)) ? (5 * Grade.precisionFactor) : parseInt(result);
+        }
+    }
+
+    calcGradeAbschlussarbeit() {
+        if (this.gradeSchriftlicheArbeit.valueShort == 0 || this.gradeVerteidigung.valueShort == 0) {
+            this.gradeAbschlussarbeit.value = 0;
+        } else {
+            if (this.gradeSchriftlicheArbeit.valueShort > (4 * Grade.precisionFactor) || this.gradeVerteidigung.valueShort > (4 * Grade.precisionFactor)) {
+                this.gradeAbschlussarbeit.value = (5 * Grade.precisionFactor);
+            } else {
+                let result = 0;
+                if (this.variant == 1) // (2/3 + 1/3) / 3
+                  result = (2 * this.gradeSchriftlicheArbeit.valueShort + this.gradeVerteidigung.valueShort) / 3;
+                else if (this.variant == 2) // (3/4 + 1/4) / 4
+                  result = (3 * this.gradeSchriftlicheArbeit.valueShort + this.gradeVerteidigung.valueShort) / 4;
+                this.gradeAbschlussarbeit.value = parseInt(result);
+            }
+        }
+        this.gradeAbschlussarbeitAsText = this.getGradeAsText(this.gradeAbschlussarbeit.value);
+    }
+
+    getGradeAsText(grade) {
+        if (grade < (1 * Grade.precisionFactor)) return " - ";
+        else if (grade <= (1.5 * Grade.precisionFactor)) return "Sehr gut";
+        else if (grade <= (2.5 * Grade.precisionFactor)) return "Gut";
+        else if (grade <= (3.5 * Grade.precisionFactor)) return "Befriedigend";
+        else if (grade <= (4 * Grade.precisionFactor)) return "Ausreichend";
+        else return "Nicht ausreichend";
+    }
+}
---
+++ js/main.js
@@ -0,0 +1,47 @@
+
+function setDefaultSettings() {
+    document.getElementById('inputGrade1-1').value = 1.3;
+    document.getElementById('inputGrade1-2').value = 1.3;
+    document.getElementById('inputGrade2-1').value = 1.3;
+    document.getElementById('inputGrade2-2').value = 1.3;
+    document.getElementById('inputVariant').value = 1;
+    updateGrades();
+}
+
+function updateGrades() {
+    // get input grades
+    const grade1 = document.getElementById('inputGrade1-1').value;
+    const grade2 = document.getElementById('inputGrade1-2').value;
+    const grade3 = document.getElementById('inputGrade2-1').value;
+    const grade4 = document.getElementById('inputGrade2-2').value;
+    const variant = document.getElementById('inputVariant').value;
+
+    // set input grades and calculate output grades
+    calc.setInputGrades(grade1, grade2, grade3, grade4, variant);
+
+    // set output grades
+    document.getElementById('outputGrade1Short').innerText = calc.gradeSchriftlicheArbeit.valueShortAsString;
+    document.getElementById('outputGrade1').innerText = calc.gradeSchriftlicheArbeit.valueAsString;
+    document.getElementById('outputGrade2Short').innerText = calc.gradeVerteidigung.valueShortAsString;
+    document.getElementById('outputGrade2').innerText = calc.gradeVerteidigung.valueAsString;
+    document.getElementById('outputGrade3Short').innerText = calc.gradeAbschlussarbeit.valueShortAsString;
+    document.getElementById('outputGrade3Text').innerText = '"' + calc.gradeAbschlussarbeitAsText + '"';
+
+    // set calculation variant
+    let result = '';
+    if (calc.variant == 1) // variant 1
+        result = '(2 × <span class="outputGrade1Short">' + calc.gradeSchriftlicheArbeit.valueShortAsString + '</span> + <span class="outputGrade2Short">' + calc.gradeVerteidigung.valueShortAsString + '</span>) / 3 = ' + calc.gradeAbschlussarbeit.valueAsString;
+    else if (calc.variant == 2) // variant 2
+        result = '(3 × <span class="outputGrade1Short">' + calc.gradeSchriftlicheArbeit.valueShortAsString + '</span> + <span class="outputGrade2Short">' + calc.gradeVerteidigung.valueShortAsString + '</span>) / 4 = ' + calc.gradeAbschlussarbeit.valueAsString;
+    document.getElementById('outputGrade3Calc').innerHTML = result;
+}
+
+function init() {
+    setDefaultSettings();
+    updateGrades();
+}
+
+/* main */
+
+let calc = new Calculation();
+init();
